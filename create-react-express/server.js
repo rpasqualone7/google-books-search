b7775ff2f8ab6581e43fc862d667b76772926a78
rpasqualone7 <highwater92@gmail.com>
2019-06-06 18:29:35 -0400

    routes, controllers, schema added

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,7 +13,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
